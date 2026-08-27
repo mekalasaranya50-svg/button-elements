fdc47328fe8988a01ffd9178c1d82476d735fbcc
mekalasaranya50-svg <mekalasaranya50@gmail.com>
2026-08-27 09:55:34 +0530

    Add event listeners for button clicks

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get the output element
+const output = document.getElementById("output");
+
+// Handle click for Button 1
+document.getElementById("btn1").addEventListener("click", function () {
+    output.innerText = "Button 1 clicked";
+});
+
+// Handle click for Button 2
+document.getElementById("btn2").addEventListener("click", function () {
+    output.innerText = "Button 2 clicked";
+});
+
+// Handle click for Button 3
+document.getElementById("btn3").addEventListener("click", function () {
+    output.innerText = "Full Stack Development - II";
+});
